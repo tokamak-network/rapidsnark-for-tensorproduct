bc8b4ddc6f5281fb078b610a4525ce8d32e17652
cd4761 <cd4761@gmail.com>
2023-12-01 15:15:02 +0900

    temp

--- testBignumber.js
+++ testBignumber.js
@@ -3,9 +3,9 @@ const { ethers } = require("ethers");
 function main () {
   const a = ethers.BigNumber.from([1,1,1,1])
   const b = ethers.BigNumber.from([2,2,2,2])
-  console.log(a.toString())
-  console.log(b.toString())
-  console.log(a.mul(b).toString())
+  console.log(a, a.toString())
+  console.log(b, b.toString())
+  console.log(a.mul(b), a.mul(b).toString())
 }
 
 main()
